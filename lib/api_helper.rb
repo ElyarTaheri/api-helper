c3ced4d3a1568085d757bb65314fcd7eba32689d
Elyar <elyar.taheri@gmail.com>
2022-09-10 19:56:59 +0430

    modifying methods

--- lib/api_helper.rb
+++ lib/api_helper.rb
@@ -5,7 +5,7 @@ require "json"
 class ApiHelper
     def initialize
 	puts "Initiating Service URL"
-	url="https://www.ebi.ac.uk/ols/api"
+	url="https://www.ebi.ac.uk/ols/api/aa"
 	@url=url
     end
 
@@ -42,19 +42,22 @@ class ApiHelper
 	puts "Fetching all ontology ids"
 	url=@url << "/ontologies?page=0&size=277"
 	puts "URL: #{url}"
-	response=HTTParty.get(url)
-	if response.code == 200
-	    jresp=JSON.parse(response.body)
-	    jresp["_embedded"]["ontologies"].each_with_index do |ont,index|
-		title = ont["config"]["title"] ? ont["config"]["title"] : "<no title>"
-		puts (index+1).to_s << ". "<< ont["ontologyId"] << " : " << title.to_s 
-	end
-        elsif response.code == 503
-                puts "503 Service Unavailable, Please try again later."
-        else
-                puts "Some Exception has occured."
-	end
+	uri = URI(url)
+	response=Net::HTTP.get_response(uri)
+
+	case response
+	    when Net::HTTPSuccess then
+		jresp=JSON.parse(response.body)
+	    	jresp["_embedded"]["ontologies"].each_with_index do |ont,index|
+			title = ont["config"]["title"] ? ont["config"]["title"] : "<no title>"
+			puts (index+1).to_s << ". "<< ont["ontologyId"] << " : " << title.to_s 
+		end
+	    else
+		puts "An Error Has Occured!"
+		http_error_handling(response)
+	    end
     end
+ 
 
     def get_ontologies_full()
 	puts "Fetching ontologies with full details"
@@ -67,19 +70,19 @@ class ApiHelper
 	    when Net::HTTPSuccess then
 		puts "Success!"
 		JSON.parse(response.body)
-	else
+	    else
 		puts "An Error Has Occured!"
 		http_error_handling(response)
-	end
+	    end
     end
 
     def get_ontology_by_id(ontology_id)  
-		puts "Fetching single ontology details"
-		puts "Input ID: " << ontology_id.to_s
-		ont_url=@url << "/ontologies/" << ontology_id.strip.to_s
-		puts "URL: #{ont_url}"		
-		uri = URI(ont_url)
-    		response=Net::HTTP.get_response(uri)
+	puts "Fetching single ontology details"
+	puts "Input ID: " << ontology_id.to_s
+	ont_url=@url << "/ontologies/" << ontology_id.strip.to_s
+	puts "URL: #{ont_url}"		
+	uri = URI(ont_url)
+	response=Net::HTTP.get_response(uri)
 	case response
 	    when Net::HTTPSuccess then
 		puts "Success!"
@@ -89,17 +92,17 @@ class ApiHelper
 		puts "Descrption: #{jresp['config']['description']}"
 		puts "Number of Terms: #{jresp['numberOfTerms']}"
 		puts "Current Status: #{jresp['status']}"
-	else
+	    else
 		puts "An Error Has Occured!"
 		http_error_handling(response)
-	end
+	    end
     end
 end
 
 
 
 helper=ApiHelper.new
-# helper.get_ontologies_id 
+helper.get_ontologies_id 
 # helper.get_ontologies_full
-helper.get_ontology_by_id("zfa")
+# helper.get_ontology_by_id("zfa")
 
