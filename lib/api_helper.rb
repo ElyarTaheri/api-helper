e2fc9b4609afe3fad65cec0c45919e37ff4296da
Elyar <elyar.taheri@gmail.com>
2022-09-10 19:40:45 +0430

    changing main method

--- lib/api_helper.rb
+++ lib/api_helper.rb
@@ -1,75 +1,105 @@
-require "http"
+require "net/http"
 require "json" 
 
+
 class ApiHelper
-	
-	def initialize
-		puts "Initiating Service URL"
-		url="http://www.ebi.ac.uk/ols/api"
-		@url=url
-	end
+    def initialize
+	puts "Initiating Service URL"
+	url="https://www.ebi.ac.uk/ols/api"
+	@url=url
+    end
 
-	def get_ontologies_id()
-		puts "Fetching all ontology ids"
-		url=@url << "/ontologies?page=0&size=277"
-		puts "URL: #{url}"
-		response=HTTParty.get(url)
-		if response.code == 200
-			jresp=JSON.parse(response.body)
-			jresp["_embedded"]["ontologies"].each_with_index do |ont,index|
-				title = ont["config"]["title"] ? ont["config"]["title"] : "<no title>"
-				puts (index+1).to_s << ". "<< ont["ontologyId"] << " : " << title.to_s 
-			end
-                elsif response.code == 503
-                        puts "503 Service Unavailable, Please try again later."
-                else
-                        puts "Some Exception has occured."
-		end
-	end
+    def http_error_handling(response)
+	case response
+	  when Net::HTTPFound then
+	    puts "Exception: Empty Ontology ID"
+	  when Net::HTTPNotFound then
+	    puts "Exception: Invalid Id, Ontology Not Found."
+	  when Net::HTTPMovedPermanently then
+	    puts "Exception: Service Has Been Moved Permanently"
+	  when Net::HTTPForbidden then
+	    puts "Exception: Access To Resource Is Forbidden"
+	  when Net::HTTPMethodNotAllowed then
+	    puts "Exception: Method Not Allowed"
+	  when Net::HTTPRequestTimeOut then
+	    puts "Exception: Request TimeOut"
+	  when Net::HTTPInternalServerError then
+	    puts "Exception: Internal Server Error"
+	  when Net::HTTPBadGateway then
+	    puts "Exception: Bad Gateway"
+	  when Net::HTTPServiceUnavailable then
+	    puts "Exception: Service Unavailable"
+	  when Net::HTTPGatewayTimeOut then
+	    puts "Exception: Gateway TimeOut"
+	  when Net::HTTPInsufficientStorage then
+	    puts "Exception: Insufficient Storage"
+	  else
+	    puts "Exception has Occured, Response Code: #{response}"
+	  end
+    end
 
-	def get_ontologies_full()
-		puts "Fetching ontologies with full details"
-		url=@url << "/ontologies?page=0&size=277"
-		puts "URL: #{url}"
-		response=HTTParty.get(@@url)
-		if response.code == 200
-			puts response.body
-		elsif response.code == 503
-                        puts "503 Service Unavailable, Please try again later."
-                else
-                        puts "Some Exception has occured."
-		end
+    def get_ontologies_id()
+	puts "Fetching all ontology ids"
+	url=@url << "/ontologies?page=0&size=277"
+	puts "URL: #{url}"
+	response=HTTParty.get(url)
+	if response.code == 200
+	    jresp=JSON.parse(response.body)
+	    jresp["_embedded"]["ontologies"].each_with_index do |ont,index|
+		title = ont["config"]["title"] ? ont["config"]["title"] : "<no title>"
+		puts (index+1).to_s << ". "<< ont["ontologyId"] << " : " << title.to_s 
 	end
+        elsif response.code == 503
+                puts "503 Service Unavailable, Please try again later."
+        else
+                puts "Some Exception has occured."
+	end
+    end
 
-	def get_ontology_by_id(ontology_id)  
+    def get_ontologies_full()
+	puts "Fetching ontologies with full details"
+	url=@url << "/ontologies?page=0&size=277"
+	puts "URL: #{url}"
+	uri = URI(url)
+	response=Net::HTTP.get_response(uri)
+	if response.code == 200
+		puts response.body
+	elsif response.code == 503
+                puts "503 Service Unavailable, Please try again later."
+        else
+                puts "Some Exception has occured."
+	end
+    end
+
+    def get_ontology_by_id(ontology_id)  
 		puts "Fetching single ontology details"
 		puts "Input ID: " << ontology_id.to_s
 		ont_url=@url << "/ontologies/" << ontology_id.strip.to_s
-		puts "URL: #{ont_url}"
-		response=HTTParty.get(ont_url)
-		puts "Response Code: #{response.code}"
-		if response.code == 200
+		puts "URL: #{ont_url}"		
+		uri = URI(ont_url)
+    		response=Net::HTTP.get_response(uri)
+
+		case response
+		    when Net::HTTPSuccess then
+			puts "Success!"
 			jresp=JSON.parse(response.body)
 			puts "ID: #{jresp['ontologyId']}"
 			puts "Title: #{jresp['config']['title']}"
 			puts "Descrption: #{jresp['config']['description']}"
 			puts "Number of Terms: #{jresp['numberOfTerms']}"
 			puts "Current Status: #{jresp['status']}"
-			# puts response.body
-		elsif response.code == 503
-			puts "503 Service Unavailable, Please try again later."
-		elsif response.code == 404
-			puts "404 Invalid Id, Ontology not found."
 		else
-			puts "Some Exception has occured."
+			puts "An Error Has Occured!"
+			http_error_handling(response)
 		end
-	end
+ 
+    end
 end
 
 
 
-# helper=ApiHelper.new
+helper=ApiHelper.new
 # helper.get_ontologies_id 
 # helper.get_ontologies_full
-# helper.get_ontology_by_id("zfa")
+helper.get_ontology_by_id("zfa")
 
